1d65269831f5df94114ca0d8f5961d920645bf54
prakashs24 <prakashs24dev@gmail.com>
2024-02-29 10:15:50 +0530

    crud1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Home'
+import Update from './Update';
+import Create from './Create';
+import Read from './Read'
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/Create' element={<Create />}></Route>
+      <Route path='/Update/:id' element={<Update />}></Route>
+      <Route path='/Read/:id' element={<Read />}></Route>
+    </Routes>
+   </BrowserRouter>
   );
 }
 
